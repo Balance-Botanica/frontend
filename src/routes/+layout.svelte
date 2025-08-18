885de0bf5bd7561e1c96306378d5e3a2860130ae
Serhii Vedmediev <serhii.vedmediev@nexus.ge>
2025-08-18 20:33:52 +0300

    Include Footer on all pages via root layout, excluding demo routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Footer from '$lib/components/Footer.svelte';
+	import { page } from '$app/stores';
 	
 	let { children } = $props();
+
+	const hiddenFooterPrefixes = ['/design-system', '/components', '/footer-demo'];
 </script>
 
 <svelte:head>
@@ -10,3 +14,7 @@
 </svelte:head>
 
 {@render children?.()}
+
+{#if !hiddenFooterPrefixes.some((p) => $page.url.pathname.startsWith(p))}
+	<Footer />
+{/if}
